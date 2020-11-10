f834968fd6296f92315d8fbef0c0d18f2c6b3fe8
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-11-10 20:54:33 +1100

    use hooks

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import Hello from './components/Hello';
 import MouseTracker from "./components/MouseTracker";
+import useURLLoader from "./hooks/useURLLoader";
 import LikeButton from "./components/LikeButton";
 import useMousePosition from "./hooks/useMousePosition";
 import logo from './logo.svg';
 import './App.css';
 
+interface IShowResult {
+  message: string;
+  status: string;
+}
 function App() {
 
   const position = useMousePosition();
+  const [data, loading] = useURLLoader('https://dog.ceo/api/breeds/image/random');
+
+  // 把 data的any类型转化成IShowResult类型。
+  const resultData = data as IShowResult;
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +26,11 @@ function App() {
           <LikeButton />
           {/*<MouseTracker />*/}
           <p>X: {position.x}, Y: {position.y}</p>
+          {loading ? <p>🐶 读取中</p>
+              : <div>
+                  <img src={resultData && resultData.message} />
+                </div>
+          }
           <a
             className="App-link"
             href="https://reactjs.org"
