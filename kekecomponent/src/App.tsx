307053e36c5c5d65dbc1661ea14b248185c0705a
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-12-11 17:06:16 +1100

    Add less && less loader

--- kekecomponent/src/App.tsx
+++ kekecomponent/src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 
-import styles from "./css/index.module.css";
+import styles from "./css/index.module.less";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className={styles.red}>123</div>
+        <div className={styles.redTheme}>123</div>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
