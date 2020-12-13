d93463e8a7014eeac51c0d32ffbddff35c005874
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-12-13 20:33:11 +1100

    Add Button test case: different props && render link

--- kekecomponent/src/component/Button/button.test.tsx
+++ kekecomponent/src/component/Button/button.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
-import Button, { ButtonType, ButtonSize, ButtonProps } from "./button";
+import { fireEvent, render } from "@testing-library/react";
+import Button, { ButtonProps, ButtonSize, ButtonType } from "./button";
 
 // 用 jest.fn() 来创建一个被监控的模拟函数
 // 用 Firing Event里面的fireEvent 来触发不同的用户事件
@@ -8,6 +8,23 @@ const defaultProps = {
   onClick: jest.fn(),
 };
 
+const disabledProps: ButtonProps = {
+  disabled: true,
+  onClick: jest.fn(),
+};
+
+const testProps: ButtonProps = {
+  btnType: ButtonType.Primary,
+  size: ButtonSize.Large,
+  className: "class",
+};
+
+const testLinkProps: ButtonProps = {
+  btnType: ButtonType.Link,
+  size: ButtonSize.Large,
+  className: "class",
+  href: "www.baidu.com",
+};
 /*
  * 1. 记得删掉React自带的testing文件，否则那个文件会被自动执行
  * 2. 记得查看package.json里面的jest-circus(删掉那一行)，否则运行不了测试
@@ -19,11 +36,14 @@ describe("test Button", () => {
   it("should render the correct default button", () => {
     // eslint-disable-next-line react/jsx-props-no-spreading
     const wrapper = render(<Button {...defaultProps}>Nice</Button>);
-    const element = wrapper.getByText("Nice");
+    /*
+     * 这里记得转换成HTMLButtonElement, 否则后面的.disable 无法使用，因为不转换拿到的是一个HTML元素， 不是一个button
+     * */
+    const element = wrapper.getByText("Nice") as HTMLButtonElement;
     expect(element).toBeInTheDocument(); // 测试当前元素是否出现在了文档中
     expect(element.tagName).toEqual("BUTTON"); // 测试是否是Button
     expect(element).toHaveClass("btn btn-default"); // 测试类名是否都正确
-
+    expect(element.disabled).toBeFalsy();
     /*
      * 1. 先模拟这个element被点击了
      * 2. 再模拟这个被触发的onClick里面的函数是否被调用了
@@ -31,7 +51,20 @@ describe("test Button", () => {
     fireEvent.click(element);
     expect(defaultProps.onClick).toHaveBeenCalled();
   });
-  it("should render the correct component based on different props", () => {});
-  it("should render a link when btnType equals link and href is provided", () => {});
+  it("should render the correct component based on different props", () => {
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    const wrapper = render(<Button {...testProps}>Nice</Button>);
+    const element = wrapper.getByText("Nice") as HTMLButtonElement;
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveClass("btn btn-lg class");
+  });
+  it("should render a link when btnType equals link and href is provided", () => {
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    const wrapper = render(<Button {...testLinkProps}>Nice</Button>);
+    const element = wrapper.getByText("Nice") as HTMLButtonElement;
+    expect(element).toBeInTheDocument();
+    expect(element.tagName).toEqual("A");
+    expect(element).toHaveClass("btn btn-link");
+  });
   it("should render disabled button when disabled is true", () => {});
 });
