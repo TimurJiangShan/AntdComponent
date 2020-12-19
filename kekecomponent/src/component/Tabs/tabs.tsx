68baa65dc8b46f9855e2068dfa2de65ed750bddf
Jiangshan Zhao <zhaojiangshancocoa@gmail.com>
2020-12-19 13:12:24 +1100

    Add Tabs style

---
+++ kekecomponent/src/component/Tabs/tabs.tsx
@@ -0,0 +1,8 @@
+import React, { CSSProperties } from "react";
+import classNames from "classnames";
+
+const Tabs = () => {
+  return <></>;
+};
+
+export default Tabs;
---
+++ kekecomponent/src/component/Tabs/tabsItem.tsx
@@ -0,0 +1,8 @@
+import React, { CSSProperties } from "react";
+import classNames from "classnames";
+
+const TabsItem = () => {
+  return <></>;
+};
+
+export default TabsItem;
